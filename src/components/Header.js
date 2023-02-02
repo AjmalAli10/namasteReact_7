b606da6c5273cd9979eb0fc74b78502818ad9a46
Ajmal Ali <officialajmalali10@gmail.com>
2023-02-02 10:35:22 +0530

    adding namste REact 07 chapter

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import "../styles/Header.css";
+import FoodLogo from "../assets/Food-Logo.png";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+const Header = () =>{
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    return(
+        <nav className="header">
+            <a href="/">
+                <img className="logo" src={FoodLogo}alt="logo" width="100" height="100"/>
+            </a>
+            <div className="nav-items">
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+            <div>
+            {isLoggedIn ? <button onClick={()=>setIsLoggedIn(false)}>LogOut</button> : <button onClick={()=>setIsLoggedIn(true)}>LogIn</button>}
+            </div>
+        </nav>
+    )
+};
+export default Header;
